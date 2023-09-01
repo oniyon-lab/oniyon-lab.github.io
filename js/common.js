2fdcd7795655bac98883221628b146c0ad4f638c
m-enomoto-bp <m-enomoto@bp.4next.jp>
2023-09-01 10:24:50 +0900

    memoページ追加、キャッシュ対策修正

--- js/common.js
+++ js/common.js
@@ -1,10 +1,10 @@
 const common = {
   version: '0.0.1'
   , loadModules: () => {
-    $('body').prepend( utils.getContents(`/module/layout.html?ver=${this.version}`) );
-    $('head').append( utils.getContents(`/module/head-common.html?ver=${this.version}`) );
-    $('head').append( `<link rel="stylesheet" href="/css/style.css?ver=${this.version}">` );
-    $('#menuArea').html( utils.getContents(`/module/menu.html?ver=${this.version}`) );
-    $('#header').html( utils.getContents(`/module/header.html?ver=${this.version}`) );
+    $('body').prepend( utils.getContents(`https://oniyon-lab.github.io/module/layout.html?ver=${this.version}`) );
+    $('head').append( utils.getContents(`https://oniyon-lab.github.io/module/head-common.html?ver=${this.version}`) );
+    $('head').append( `<link rel="stylesheet" href="https://oniyon-lab.github.io/css/style.css?ver=${this.version}">` );
+    $('#menuArea').html( utils.getContents(`https://oniyon-lab.github.io/module/menu.html?ver=${this.version}`) );
+    $('#header').html( utils.getContents(`https://oniyon-lab.github.io/module/header.html?ver=${this.version}`) );
   }
 };
