9ca965b9683632d25a4a67917b64b2f3d7b9eaa2
m-enomoto-bp <m-enomoto@bp.4next.jp>
2023-09-01 10:14:03 +0900

    キャッシュ対策

--- js/common.js
+++ js/common.js
@@ -1,8 +1,10 @@
 const common = {
-  loadModules: () => {
-    $('body').prepend( utils.getContents('/module/layout.html') );
-    $('head').append( utils.getContents('/module/head-common.html') );
-    $('#menuArea').html( utils.getContents('/module/menu.html') );
-    $('#header').html( utils.getContents('/module/header.html') );
+  version: '0.0.1'
+  , loadModules: () => {
+    $('body').prepend( utils.getContents(`/module/layout.html?ver=${this.version}`) );
+    $('head').append( utils.getContents(`/module/head-common.html?ver=${this.version}`) );
+    $('head').append( `<link rel="stylesheet" href="/css/style.css?ver=${this.version}">` );
+    $('#menuArea').html( utils.getContents(`/module/menu.html?ver=${this.version}`) );
+    $('#header').html( utils.getContents(`/module/header.html?ver=${this.version}`) );
   }
 };
