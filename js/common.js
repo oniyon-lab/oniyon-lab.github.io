a7570fc6654d662f5975b2f46d83bc9e00f095ac
m-enomoto-bp <m-enomoto@bp.4next.jp>
2023-09-01 13:53:31 +0900

    メニュー調整

--- js/common.js
+++ js/common.js
@@ -5,6 +5,7 @@ const common = {
     $('head').append( utils.getContents(`/module/head-common.html?ver=${common.version}`) );
     $('head').append( `<link rel="stylesheet" href="/css/style.css?ver=${common.version}">` );
     $('#menuArea').html( utils.getContents(`/module/menu.html?ver=${common.version}`) );
+    $(`.pure-menu-item a[href="${location.pathname}"]`).parent().addClass('menu-item-divided pure-menu-selected');
     $('#header').html( utils.getContents(`/module/header.html?ver=${common.version}`) );
   }
 };
