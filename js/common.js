5b2783994d21d79d7cde5ed77f2b1d2e8713d5ab
m-enomoto-bp <m-enomoto@bp.4next.jp>
2023-09-01 10:47:37 +0900

    バージョン更新

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 const common = {
-  version: '0.0.1'
+  version: '0.0.2'
   , loadModules: () => {
     $('body').prepend( utils.getContents(`/module/layout.html?ver=${common.version}`) );
     $('head').append( utils.getContents(`/module/head-common.html?ver=${common.version}`) );
